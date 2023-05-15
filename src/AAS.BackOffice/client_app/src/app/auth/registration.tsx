df02c5aefb5ae12aa653e882b131fbdf92c231c1
GrishinMatvei <batska.typok@mail.ru>
2023-05-15 18:15:05 +0300

    Добавил текст с описанием сайта на страницу регистрации и авторизации

--- src/AAS.BackOffice/client_app/src/app/auth/registration.tsx
+++ src/AAS.BackOffice/client_app/src/app/auth/registration.tsx
@@ -21,91 +21,136 @@ export const Registration = () => {
     }
 
     return (
-        <Container maxWidth={false}
-                   sx={{
-                       display: 'flex',
-                       flexDirection: 'column',
-                       height: '100%',
-                   }}>
-            <Paper
-                elevation={3}
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '25%',
-                    minWidth: '450px',
-                    minHeight: '300px',
-                    padding: 2,
-                    margin: 'auto',
-                    gap: 2
-                }}>
-                <Typography variant='h5'>
-                    Регистрация
-                </Typography>
-                <Box sx={{display: "flex", gap: 2}}>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            height: '100%',
+            width: '100%'
+        }}>
+            <Container sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}>
+                <Paper
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '25%',
+                        minWidth: '800px',
+                        minHeight: '300px',
+                        padding: 2,
+                        margin: 'auto',
+                        gap: 2
+                    }}>
+                    <Typography gutterBottom>
+                        Добро пожаловать на наш сайт! Мы - команда талантливых студентов, увлеченных разработкой
+                        программного обеспечения, готовых принять вашу заявку на создание проекта любой сложности.
+                    </Typography>
+                    <Typography gutterBottom>
+                        Мы понимаем, что каждый проект уникален и требует индивидуального подхода, поэтому мы тщательно
+                        анализируем требования наших клиентов и работаем в тесном сотрудничестве с ними, чтобы
+                        предоставить наилучшее решение, которое будет соответствовать их потребностям. Мы используем
+                        современные технологии и методики разработки, что позволяет нам создавать качественное
+                        программное обеспечение в срок и в рамках бюджета.
+                    </Typography>
+                    <Typography gutterBottom>
+                        Мы ценим открытость и прозрачность в отношениях с нашими клиентами, поэтому вы можете быть
+                        уверены, что мы будем держать вас в курсе всех этапов разработки вашего проекта. Мы также готовы
+                        предоставить бесплатную консультацию и помощь в выборе технологий, если у вас возникнут вопросы.
+                    </Typography>
+                    <Typography gutterBottom>
+                        Свяжитесь с нами, чтобы оставить заявку на разработку ПО. Мы готовы принять любые вызовы и
+                        создать для вас программное обеспечение высокого уровня.
+                    </Typography>
+                </Paper>
+            </Container>
+            <Container maxWidth={false}
+                       sx={{
+                           display: 'flex',
+                           flexDirection: 'column',
+                           height: '100%',
+                       }}>
+                <Paper
+                    elevation={3}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '25%',
+                        minWidth: '450px',
+                        minHeight: '300px',
+                        padding: 2,
+                        margin: 'auto',
+                        gap: 2
+                    }}>
+                    <Typography variant='h5'>
+                        Регистрация
+                    </Typography>
+                    <Box sx={{display: "flex", gap: 2}}>
+                        <InputForm
+                            type="text"
+                            label="Имя"
+                            required
+                            placeholder="Введите имя"
+                            value={userRegistrationBlank.firstName}
+                            onChange={(firstName) => setUserRegistrationBlank(blank => ({...blank, firstName}))}/>
+                        <InputForm
+                            type="text"
+                            required
+                            label="Фамилия"
+                            placeholder="Введите фамилию"
+                            value={userRegistrationBlank.lastName}
+                            onChange={(lastName) => setUserRegistrationBlank(blank => ({...blank, lastName}))}/>
+                    </Box>
                     <InputForm
                         type="text"
-                        label="Имя"
-                        required
-                        placeholder="Введите имя"
-                        value={userRegistrationBlank.firstName}
-                        onChange={(firstName) => setUserRegistrationBlank(blank => ({...blank, firstName}))}/>
+                        label="Отчество"
+                        placeholder="Введите отчество"
+                        value={userRegistrationBlank.middleName}
+                        onChange={(middleName) => setUserRegistrationBlank(blank => ({...blank, middleName}))}/>
                     <InputForm
                         type="text"
                         required
-                        label="Фамилия"
-                        placeholder="Введите фамилию"
-                        value={userRegistrationBlank.lastName}
-                        onChange={(lastName) => setUserRegistrationBlank(blank => ({...blank, lastName}))}/>
-                </Box>
-                <InputForm
-                    type="text"
-                    label="Отчество"
-                    placeholder="Введите отчество"
-                    value={userRegistrationBlank.middleName}
-                    onChange={(middleName) => setUserRegistrationBlank(blank => ({...blank, middleName}))}/>
-                <InputForm
-                    type="text"
-                    required
-                    label="Email"
-                    placeholder="Введите email"
-                    value={userRegistrationBlank.email}
-                    onChange={(email) => setUserRegistrationBlank(blank => ({...blank, email}))}/>
-                <InputForm
-                    type="text"
-                    required
-                    label="Телефон"
-                    placeholder="Введите телефон"
-                    value={userRegistrationBlank.phoneNumber}
-                    onChange={(phoneNumber) => setUserRegistrationBlank(blank => ({...blank, phoneNumber}))}/>
-                <InputForm
-                    type="password"
-                    label="Пароль"
-                    placeholder="Введите пароль"
-                    value={userRegistrationBlank.password}
-                    onChange={(password) => setUserRegistrationBlank(blank => ({...blank, password}))}/>
-                <InputForm
-                    type="password"
-                    label="Повторите пароль"
-                    placeholder="Введите пароль"
-                    value={userRegistrationBlank.rePassword}
-                    onChange={(rePassword) => setUserRegistrationBlank(blank => ({...blank, rePassword}))}/>
-                <Tooltip title='Зарегистрироваться'>
-                    <Button variant="outlined" onClick={() => registerUser()}>
-                        Зарегистрироваться
-                    </Button>
-                </Tooltip>
-                <Tooltip title='Перейти на страницу авторизации'>
-                    <Link onClick={() => navigate(AuthLinks.authentification)}
-                          sx={{
-                              fontSize: '15px',
-                              width: '30%',
-                              marginLeft: '79%'
-                          }}>
-                        Авторизация
-                    </Link>
-                </Tooltip>
-            </Paper>
-        </Container>
+                        label="Email"
+                        placeholder="Введите email"
+                        value={userRegistrationBlank.email}
+                        onChange={(email) => setUserRegistrationBlank(blank => ({...blank, email}))}/>
+                    <InputForm
+                        type="text"
+                        required
+                        label="Телефон"
+                        placeholder="Введите телефон"
+                        value={userRegistrationBlank.phoneNumber}
+                        onChange={(phoneNumber) => setUserRegistrationBlank(blank => ({...blank, phoneNumber}))}/>
+                    <InputForm
+                        type="password"
+                        label="Пароль"
+                        placeholder="Введите пароль"
+                        value={userRegistrationBlank.password}
+                        onChange={(password) => setUserRegistrationBlank(blank => ({...blank, password}))}/>
+                    <InputForm
+                        type="password"
+                        label="Повторите пароль"
+                        placeholder="Введите пароль"
+                        value={userRegistrationBlank.rePassword}
+                        onChange={(rePassword) => setUserRegistrationBlank(blank => ({...blank, rePassword}))}/>
+                    <Tooltip title='Зарегистрироваться'>
+                        <Button variant="outlined" onClick={() => registerUser()}>
+                            Зарегистрироваться
+                        </Button>
+                    </Tooltip>
+                    <Tooltip title='Перейти на страницу авторизации'>
+                        <Link onClick={() => navigate(AuthLinks.authentification)}
+                              sx={{
+                                  fontSize: '15px',
+                                  width: '30%',
+                                  marginLeft: '79%'
+                              }}>
+                            Авторизация
+                        </Link>
+                    </Tooltip>
+                </Paper>
+            </Container>
+        </Box>
     )
 }
